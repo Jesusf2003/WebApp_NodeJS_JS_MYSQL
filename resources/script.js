98ef08ebcd68b643b6d43172007efe4a5ab3243c
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2021-12-06 22:42:48 -0500

    Se ha añadido la validación de campos vacíos.

--- resources/script.js
+++ resources/script.js
@@ -17,30 +17,36 @@ formArticulo.addEventListener('submit',
     (e) => {
         e.preventDefault();
         if (opcion == 'crear') {
-            fetch(
-                url,
-                {
-                    method: 'POST',
-                    headers: {
-                        'content-Type':'application/json'
-                    },
-                    body: JSON.stringify(
-                        {
-                            nameord: nameord.value,
-                            emaord: emaord.value,
-                            celord: celord.value,
-                            foonamord: foonamord.value,
-                            msgcord: msgcord.value
-                        }
-                    )
-                }
-            )
-            .then(
-                response => response.json()
-            )
-            .then(
-                response => location.reload()
-            );
+            if (nameord.value == "" || emaord.value == "" || celord.value == "" || foonamord.value == "" || msgcord.value == "") {
+                alert("Asegúrese de que todos los campos estén completos");
+                return false;
+            } else {
+                console.log("Todos los campos están completos");
+                fetch(
+                    url,
+                    {
+                        method: 'POST',
+                        headers: {
+                            'content-Type':'application/json'
+                        },
+                        body: JSON.stringify(
+                            {
+                                nameord: nameord.value,
+                                emaord: emaord.value,
+                                celord: celord.value,
+                                foonamord: foonamord.value,
+                                msgcord: msgcord.value
+                            }
+                        )
+                    }
+                )
+                .then(
+                    response => response.json()
+                )
+                .then(
+                    response => location.reload()
+                );
+            }
         } else if(opcion == 'editar'){
             console.log("Activado el ");
         }
