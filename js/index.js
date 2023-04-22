a88da1c19aa4d3dd79a76caef02cb026e93887e1
Jesusf2003 <fernando.canales@vallegrande.edu.pe>
2023-04-22 15:50:54 -0500

    Ahora es posible guardar datos modificados

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ const namecli = document.getElementById("NAMECLI");
 const lnamecli = document.getElementById("LNAMECLI");
 const cellcli = document.getElementById("CELLCLI");
 
-var idValue = 1;
+var idValue;
 var option = '';
 
 btnCrear.addEventListener("click", () => {
@@ -34,15 +34,18 @@ async function showContent() {
     idCell.innerHTML = data[i].IDCLI;
 
     const nameCell = newRow.insertCell(1);
-    nameCell.innerHTML = data[i].LNAMECLI + ", " + data[i].NAMECLI;
+    nameCell.innerHTML = data[i].NAMECLI;
 
-    const cellCell = newRow.insertCell(2);
+    const lnameCell = newRow.insertCell(2);
+    lnameCell.innerHTML = data[i].LNAMECLI;
+
+    const cellCell = newRow.insertCell(3);
     cellCell.innerHTML = data[i].CELLCLI;
 
-    const actionsCell = newRow.insertCell(3);
+    const actionsCell = newRow.insertCell(4);
     actionsCell.innerHTML = `
       <a onClick='eliminar(this)'>Eliminar</a>
-      <a>Editar</a>
+      <a onClick='passData(this)'>Editar</a>
     `;
   }
 }
@@ -62,31 +65,47 @@ function eliminar(id) {
   fetch(MAIN_PATH + "/id/"+ dataSelected, {method: 'DELETE'});
 }
 
-showContent();
-
-async function showContentById(id) {
-  const response = await fetch(MAIN_PATH + "/id/"+ id, {method: 'GET', headers: {'Content-Type': 'application/json'}});
+function passData(id) {
+  let table = id.parentElement.parentElement;
+  let response = [table].map(
+    tr => {
+      return {
+        idcli: tr.children[0].innerText,
+        namecli: tr.children[1].innerText,
+        lnamecli: tr.children[2].innerText,
+        cellcli: tr.children[3].innerText
+      };
+    }
+  );
+  console.log(response);
+  this.idValue = response[0].idcli;
+  document.getElementById("NAMECLI").value = response[0].namecli;
+  document.getElementById("LNAMECLI").value = response[0].lnamecli;
+  document.getElementById("CELLCLI").value = response[0].cellcli;
 }
+
+showContent();
 
 formTag.addEventListener('submit',
   (e) => {
     e.preventDefault();
     if (option == 'crear') {
-      if (namecli.value == "" || lnamecli.value == "" || cellcli.value == "") {
+      if (namecli.value == "" && lnamecli.value == "" && cellcli.value == "") {
         alert("Asegúrese de que todos los campos estén completos");
         return false;
       } else {
         console.log("Todos los campos fueron llenados");
+        let data = {
+          NAMECLI: namecli.value,
+          LNAMECLI: lnamecli.value,
+          CELLCLI: cellcli.value
+        }
         fetch(MAIN_PATH, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify({
-            NAMECLI: namecli.value,
-            LNAMECLI: lnamecli.value,
-            CELLCLI: cellcli.value
-          })
+          body: JSON.stringify(data)
         })
         .then(
           response => response.json()
@@ -97,23 +116,30 @@ formTag.addEventListener('submit',
       }
     } else if(option == 'editar') {
       console.log("Escogió editar");
-      fetch(MAIN_PATH + '/id/'+ idValue, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
+      if (namecli.value == "" || lnamecli.value == "" || cellcli.value == "") {
+        alert("Asegúrese de que todos los campos estén completos");
+        return false;
+      } else {
+        let data = {
           NAMECLI: namecli.value,
           LNAMECLI: lnamecli.value,
           CELLCLI: cellcli.value
+        }
+        console.log(data);
+        fetch(MAIN_PATH + '/id/'+ idValue, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
         })
-      })
-      .then(
-        response => response.json()
-      )
-      .then(
-        response => location.reload()
-      );
+        .then(
+          response => response.json()
+        )
+        .then(
+          response => location.reload()
+        );
+      }
     }
   }
 );
